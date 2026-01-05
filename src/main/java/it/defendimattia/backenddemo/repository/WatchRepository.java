46189bac51fce348d2bb4912e23b1a7e21c9daf9
defendimattia <defendi.mattia92@gmail.com>
2026-01-05 16:09:02 +0100

    Refactor WatchService and WatchRepository to remove unused search methods; add WatchSpecification for dynamic querying

--- src/main/java/it/defendimattia/backenddemo/repository/WatchRepository.java
+++ src/main/java/it/defendimattia/backenddemo/repository/WatchRepository.java
@@ -1,51 +1,10 @@
 package it.defendimattia.backenddemo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import it.defendimattia.backenddemo.model.Watch;
 
-public interface WatchRepository extends JpaRepository<Watch, Integer> {
-
-    public List<Watch> findByBrandIgnoreCaseContaining(String brand);
-
-    public List<Watch> findByModelIgnoreCaseContaining(String model);
-
-    public List<Watch> findByCaseMaterialIgnoreCaseContaining(String caseMaterial);
-
-    public List<Watch> findByStrapMaterialIgnoreCaseContaining(String strapMaterial);
-
-    public List<Watch> findByMovementTypeIgnoreCaseContaining(String movementType);
-
-    public List<Watch> findByWaterResistanceGreaterThanEqual(Short waterResistance);
-
-    public List<Watch> findByCaseDiameterGreaterThanEqualAndCaseDiameterLessThan(Double min, Double max);
-
-    public List<Watch> findByCaseDiameterGreaterThanEqual(Double diameter);
-
-    public List<Watch> findByCaseDiameterLessThanEqual(Double diameter);
-
-    public List<Watch> findByCaseThicknessGreaterThanEqualAndCaseThicknessLessThan(Double min, Double max);
-
-    public List<Watch> findByCaseThicknessGreaterThanEqual(Double caseThickness);
-
-    public List<Watch> findByCaseThicknessLessThanEqual(Double caseThickness);
-
-    public List<Watch> findByBandWidthGreaterThanEqualAndBandWidthLessThan(Double min, Double max);
-
-    public List<Watch> findByBandWidthGreaterThanEqual(Double bandWidth);
-
-    public List<Watch> findByBandWidthLessThanEqual(Double bandWidth);
-
-    public List<Watch> findByDialColorIgnoreCaseContaining(String dialColor);
-
-    public List<Watch> findByCrystalMaterialIgnoreCaseContaining(String crystalMaterial);
-
-    public List<Watch> findByComplicationsIgnoreCaseContaining(String complications);
-
-    public List<Watch> findByPowerReserveGreaterThanEqual(Short powerReserve);
-
-    public List<Watch> findByPriceLessThanEqual(Integer bprice);
+public interface WatchRepository extends JpaRepository<Watch, Integer>, JpaSpecificationExecutor<Watch> {
 
 }
---
+++ src/main/java/it/defendimattia/backenddemo/specification/WatchSpecification.java
@@ -0,0 +1,223 @@
+package it.defendimattia.backenddemo.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import it.defendimattia.backenddemo.model.Watch;
+
+public class WatchSpecification {
+
+    public static Specification<Watch> brandContains(String brand) {
+        return (root, query, cb) -> {
+
+            if (brand == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("brand")),
+                    "%" + brand.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> modelContains(String model) {
+        return (root, query, cb) -> {
+
+            if (model == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("model")),
+                    "%" + model.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> caseMaterialContains(String caseMaterial) {
+        return (root, query, cb) -> {
+
+            if (caseMaterial == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("caseMaterial")),
+                    "%" + caseMaterial.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> strapMaterialContains(String strapMaterial) {
+        return (root, query, cb) -> {
+
+            if (strapMaterial == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("strapMaterial")),
+                    "%" + strapMaterial.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> movementTypeContains(String movementType) {
+        return (root, query, cb) -> {
+
+            if (movementType == null) {
+                return null;
+            }
+
+            return cb.like(cb.lower(root.get("movementType")),
+                    "%" + movementType.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> waterResistanceGreaterThanLike(Short waterResistance) {
+        return (root, query, cb) -> {
+
+            if (waterResistance == null) {
+                return null;
+            }
+
+            return cb.greaterThanOrEqualTo(root.get("waterResistance"), waterResistance);
+        };
+    }
+
+    public static Specification<Watch> caseDiameterLike(Double min, Double max) {
+        return (root, query, cb) -> {
+
+            if (min == null || max == null) {
+                return null;
+            }
+
+            return cb.and(
+                    cb.greaterThanOrEqualTo(root.get("caseDiameter"), min),
+                    cb.lessThanOrEqualTo(root.get("caseDiameter"), max));
+        };
+    }
+
+    public static Specification<Watch> diameterGreaterThanEqual(Double diameter) {
+        return (root, query, cb) -> {
+
+            if (diameter == null) {
+                return null;
+            }
+
+            return cb.greaterThanOrEqualTo(root.get("diameter"), diameter);
+        };
+    }
+
+    public static Specification<Watch> diameterLessThanEqual(Double diameter) {
+        return (root, query, cb) -> {
+
+            if (diameter == null) {
+                return null;
+            }
+
+            return cb.lessThanOrEqualTo(root.get("diameter"), diameter);
+        };
+    }
+
+    public static Specification<Watch> caseThicknessGreaterThanEqual(Double caseThickness) {
+        return (root, query, cb) -> {
+
+            if (caseThickness == null) {
+                return null;
+            }
+
+            return cb.greaterThanOrEqualTo(root.get("caseThickness"), caseThickness);
+        };
+    }
+
+    public static Specification<Watch> caseThicknessLessThanEqual(Double caseThickness) {
+        return (root, query, cb) -> {
+
+            if (caseThickness == null) {
+                return null;
+            }
+
+            return cb.lessThanOrEqualTo(root.get("caseThickness"), caseThickness);
+        };
+    }
+
+    public static Specification<Watch> bandWidthGreaterThanEqual(Double bandWidth) {
+        return (root, query, cb) -> {
+
+            if (bandWidth == null) {
+                return null;
+            }
+
+            return cb.greaterThanOrEqualTo(root.get("bandWidth"), bandWidth);
+        };
+    }
+
+    public static Specification<Watch> bandWidthLessThanEqual(Double bandWidth) {
+        return (root, query, cb) -> {
+
+            if (bandWidth == null) {
+                return null;
+            }
+
+            return cb.lessThanOrEqualTo(root.get("bandWidth"), bandWidth);
+        };
+    }
+
+    public static Specification<Watch> dialColorContaining(String dialColor) {
+        return (root, query, cb) -> {
+
+            if (dialColor == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("dialColor")),
+                    "%" + dialColor.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> crystalMaterialContaining(String crystalMaterial) {
+        return (root, query, cb) -> {
+
+            if (crystalMaterial == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("crystalMaterial")),
+                    "%" + crystalMaterial.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> complicationsContaining(String complications) {
+        return (root, query, cb) -> {
+
+            if (complications == null) {
+                return null;
+            }
+
+            return cb.like(
+                    cb.lower(root.get("complications")),
+                    "%" + complications.toLowerCase() + "%");
+        };
+    }
+
+    public static Specification<Watch> powerReserveGreaterThanEqual(Short powerReserve) {
+        return (root, query, cb) -> {
+
+            if (powerReserve == null) {
+                return null;
+            }
+
+            return cb.greaterThanOrEqualTo(root.get("powerReserve"), powerReserve);
+        };
+    }
+
+    public static Specification<Watch> priceGreaterThanEqual(Integer price) {
+        return (root, query, cb) -> {
+
+            if (price == null) {
+                return null;
+            }
+
+            return cb.lessThanOrEqualTo(root.get("price"), price);
+        };
+    }
+}
