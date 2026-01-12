9d85ec667b707312500975d90a55b5c1a468e53a
defendimattia <defendi.mattia92@gmail.com>
2026-01-12 11:16:04 +0100

    Enhance Watch repository with JavaDoc

--- src/main/java/it/defendimattia/backenddemo/repository/WatchRepository.java
+++ src/main/java/it/defendimattia/backenddemo/repository/WatchRepository.java
@@ -5,6 +5,20 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import it.defendimattia.backenddemo.model.Watch;
 
+/**
+ * Repository for managing luxury watches.
+ * 
+ * <p>
+ * Extends {@link JpaRepository} to provide basic CRUD operations
+ * and {@link JpaSpecificationExecutor} to support advanced searches using
+ * Specifications.
+ * </p>
+ * 
+ * <p>
+ * The managed entity is {@link Watch}.
+ * </p>
+ * 
+ */
 public interface WatchRepository extends JpaRepository<Watch, Integer>, JpaSpecificationExecutor<Watch> {
 
 }
